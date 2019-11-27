21e187ea0ec5bb6ea26ff64de536e1b6e42dcdaf
quantl12113 <quantl12113@gmail.com>
2019-11-27 21:45:10 +0700

    update cart, fix admin

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
 
   scope module: "admin", path: "admin", as: "admin" do
-    get "/home", to: "homes#index"
+    get "/home", to: "users#index"
     get "/new_user", to: "users#new_user"
     get "/edit_user", to: "users#edit_user"
     get "/show_user", to: "users#show_user"
